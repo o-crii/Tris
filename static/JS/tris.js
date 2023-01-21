2f7fe5599583231fc040125bab800a21d21dbd31
o-crii <cristianpesola@gmail.com>
2023-01-21 09:48:13 +0100

    da ultimare

---
+++ static/JS/tris.js
@@ -0,0 +1,19 @@
+let links = document.querySelectorAll(".gridBtn"),
+			currentPlayer = 1,
+    cellIsOccupied = true
+
+
+		for(let i = 0; i < links.length; i++){
+			let link = links[i]
+			link.addEventListener("click", function(e){
+				e.preventDefault()
+	
+        cellIsOccupied = e.currentTarget.classList.contains("player2") || e.currentTarget.classList.contains("player1")	;			
+        
+        console.log(cellIsOccupied)
+				if(!cellIsOccupied){
+				
+        e.currentTarget.classList.toggle("player1")
+        }
+			})
+		}
